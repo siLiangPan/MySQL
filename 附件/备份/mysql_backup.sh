e60eedeefc43f4247ab84ababd2a49baee6aed37
siLiangPan <425274873@qq.com>
2017-11-07 14:01:13 +0800

    0

--- 附件/备份/mysql_backup.sh
+++ 附件/备份/mysql_backup.sh
@@ -15,95 +15,45 @@ fi
 #echo $Ymd delete OK!
 
 
-rm -rf "./MysqlBackUp/mysql_*_$Ymd.sql"
+rm -rf ./MysqlBackUp/mysql_*.sql
 echo $Ymd delete OK!
 
 ##################
 # backup
 ##################
-mysqldump   -h192.168.7.133 -P9906 --databases tdx -uroot -p123456 -q --default-character-set=utf8 > "./MysqlBackUp/mysql_tdx_$Ymd.sql" 
+function mysql_backup {
+database_name=$1
+mysqldump   -h192.168.7.133 -P9906 --databases ${database_name} -uroot -p123456 -q --default-character-set=utf8 > "./MysqlBackUp/mysql_${database_name}_${Ymd}.sql" 
 if [ $? -ne 0 ]
 then
-echo $Ymd backup 133 tdx failed! 
+echo ${Ymd} backup 133 ${database_name} failed! 
 else
-echo $Ymd backup 133 tdx succeed! 
+echo ${Ymd} backup 133 ${database_name} succeed! 
 fi
-
-mysqldump   -h192.168.7.133 -P9906 --databases pul -uroot -p123456 -q --default-character-set=utf8 > "./MysqlBackUp/mysql_pul_$Ymd.sql" 
-if [ $? -ne 0 ]
-then
-echo $Ymd backup 133 pul failed! 
-else
-echo $Ymd backup 133 pul succeed! 
-fi
-
-mysqldump   -h192.168.7.133 -P9906 --databases new_pul -uroot -p123456 -q --default-character-set=utf8 > "./MysqlBackUp/mysql_new_pul_$Ymd.sql" 
-if [ $? -ne 0 ]
-then
-echo $Ymd backup 133 new_pul failed! 
-else
-echo $Ymd backup 133 new_pul succeed! 
-fi
-
-mysqldump   -h192.168.7.133 -P9906 --databases new_sso -uroot -p123456 -q --default-character-set=utf8 > "./MysqlBackUp/mysql_new_sso_$Ymd.sql" 
-if [ $? -ne 0 ]
-then
-echo $Ymd backup 133 new_sso failed! 
-else
-echo $Ymd backup 133 new_sso succeed! 
-fi
-
-mysqldump   -h192.168.7.133 -P9906 --databases tdx_mobile -uroot -p123456 -q --default-character-set=utf8 > "./MysqlBackUp/mysql_tdx_mobile_$Ymd.sql" 
-if [ $? -ne 0 ]
-then
-echo $Ymd backup 133 tdx_mobile failed! 
-else
-echo $Ymd backup 133 tdx_mobile succeed! 
-fi 
+}
 
 ##################
 # recover
 ##################
-mysql -h192.168.7.132 -P9906 -uroot -p123456 -q  < "./MysqlBackUp/mysql_tdx_$Ymd.sql" 
+function mysql_recover {
+database_name=$1
+mysql -h192.168.7.132 -P9906 -uroot -p123456 -q  < "./MysqlBackUp/mysql_${database_name}_${Ymd}.sql" 
 if [ $? -ne 0 ]
 then
-echo $Ymd recover 132 tdx failed! 
+echo ${Ymd} recover 132 ${database_name} failed! 
 else
-echo $Ymd recover 132 tdx succeed! 
-fi 
+echo ${Ymd} recover 132 ${database_name} succeed! 
+fi
+}
 
-mysql -h192.168.7.132 -P9906 -uroot -p123456 -q  < "./MysqlBackUp/mysql_pul_$Ymd.sql" 
-if [ $? -ne 0 ]
-then
-echo $Ymd recover 132 pul failed! 
-else
-echo $Ymd recover 132 pul succeed! 
-fi 
-
-mysql -h192.168.7.132 -P9906 -uroot -p123456 -q  < "./MysqlBackUp/mysql_new_pul_$Ymd.sql" 
-if [ $? -ne 0 ]
-then
-echo $Ymd recover 132 new_pul failed! 
-else
-echo $Ymd recover 132 new_pul succeed! 
-fi 
-
-mysql -h192.168.7.132 -P9906 -uroot -p123456 -q  < "./MysqlBackUp/mysql_new_sso_$Ymd.sql" 
-if [ $? -ne 0 ]
-then
-echo $Ymd recover 132 new_sso failed! 
-else
-echo $Ymd recover 132 new_sso succeed! 
-fi 
-
-mysql -h192.168.7.132 -P9906 -uroot -p123456 -q  < "./MysqlBackUp/mysql_tdx_mobile_$Ymd.sql" 
-if [ $? -ne 0 ]
-then
-echo $Ymd recover 132 tdx_mobile failed! 
-else
-echo $Ymd recover 132 tdx_mobile succeed! 
-fi 
-
+##################
+# work start
+##################
+arr_database=("tdx" "pul" "new_pul" "new_sso" "tdx_mobile")
+for v in ${arr_database[@]}; do
+    mysql_backup $v
+    mysql_recover $v
+done
 echo $Ymd backup ok!
 
 #if test -f "./MysqlBackUp/mysql_xsd_tdx_tq_$Ymd.sql" ; then
